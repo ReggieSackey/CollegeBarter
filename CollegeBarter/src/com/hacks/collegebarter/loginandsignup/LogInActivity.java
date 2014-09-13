d9baf56002f1da7bf65cb0d736f51152a6cb162f
Albert Owusu Asare <owusuasa@grinnell.edu>
2014-09-13 13:48:47 -0500

    Add onclick button functionality to signUp button for LoginActivity Add user signup functionality to SignUpActivity

--- CollegeBarter/src/com/hacks/collegebarter/loginandsignup/LogInActivity.java
+++ CollegeBarter/src/com/hacks/collegebarter/loginandsignup/LogInActivity.java
@@ -1,24 +1,20 @@
 package com.hacks.collegebarter.loginandsignup;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.hacks.collegebarter.R;
 import com.parse.Parse;
-import com.parse.ParseException;
-import com.parse.ParseUser;
-import com.parse.SignUpCallback;
 
 public class LogInActivity extends Activity {
-
 	/* Variables storing widget id's */
-	
+
 	EditText userNameField;
 	EditText passwordField;
 	Button signUpButton;
@@ -26,56 +22,33 @@ public class LogInActivity extends Activity {
 	String userNameText;
 	String passwordText;
 
-	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_log_in);
-		
+
 		// set widget id's
 		userNameField = (EditText) findViewById(R.id.usernameField);
 		passwordField = (EditText) findViewById(R.id.passwordField);
 		signUpButton = (Button) findViewById(R.id.signUpButton);
 		logInButton = (Button) findViewById(R.id.logInId);
-		
-		//Parse Initialize
-		 Parse.initialize(this, "yGAHbDZTw9xuh1NHC5YMLoif5u9qgYoOGak2nd62", "BY79vcfUFtS9WTRCdg9Vf2ovnLnYzPnYk2waPwbq");
-		 
-		
- 
-		 /* When signUp button is clicked */
-		 
-		 signUpButton.setOnClickListener(new View.OnClickListener(){
-			 public void onClick(View v){
-				 // create a new Parse User
-			ParseUser newUser = new ParseUser();
-			
-			// get the strings stored in userName and password field
-			
-			userNameText = userNameField.getText().toString();
-			passwordText = passwordField.getText().toString();
-			
-			//set parseuser name and password fields
-			newUser.setUsername(userNameText);
-			newUser.setPassword(passwordText);
-			
-		  /* save username and password on server, background process */
-			
-			newUser.signUpInBackground(new SignUpCallback(){
-				public void done(ParseException e){
-					if (e == null){
-						Toast.makeText(LogInActivity.this, "Going to app", Toast.LENGTH_SHORT).show();
-					} //if sigUp passed
-					else if(e.getCode() == ParseException.USERNAME_TAKEN){
-				        Toast.makeText(LogInActivity.this, "Username already taken", Toast.LENGTH_LONG).show();	
-					}//userName is taken
-				}
-	     	});	 
-		 }
-	 });
-		 	 
+
+		// Parse Initialize
+		Parse.initialize(this, "yGAHbDZTw9xuh1NHC5YMLoif5u9qgYoOGak2nd62",
+				"BY79vcfUFtS9WTRCdg9Vf2ovnLnYzPnYk2waPwbq");
+
+		/* When signUp button is clicked */
+
+		signUpButton.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				
+				//switch to new activity
+				Intent myIntent = new Intent(LogInActivity.this, SignUpActivity.class);
+		        startActivity(myIntent);
+			}
+		});
+
 	}
-	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- CollegeBarter/src/com/hacks/collegebarter/loginandsignup/SignUpActivity.java
+++ CollegeBarter/src/com/hacks/collegebarter/loginandsignup/SignUpActivity.java
@@ -5,28 +5,38 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.hacks.collegebarter.R;
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 public class SignUpActivity extends Activity {
 
-	
- /* Variables storing widget id's */
-	
+
+
+	/* Variables storing widget id's */
+
 	EditText userNameField;
-	EditText emailField; 
+	EditText emailField;
 	EditText passwordField;
-	String userNameText;
+	String usernameText;
 	String emailText;
 	String passwordText;
-	
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sign_up);
-		
-		
+		// Parse Initialize
+				Parse.initialize(this, "yGAHbDZTw9xuh1NHC5YMLoif5u9qgYoOGak2nd62",
+						"BY79vcfUFtS9WTRCdg9Vf2ovnLnYzPnYk2waPwbq");
+		userNameField = (EditText) findViewById(R.id.signUpUserNameText);
+		emailField = (EditText) findViewById(R.id.signUpEmailText);
+		passwordField = (EditText) findViewById(R.id.signUpPasswordText);
+
 	}
 
 	@Override
@@ -42,9 +52,52 @@ public class SignUpActivity extends Activity {
 		// automatically handle clicks on the Home/Up button, so long
 		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
-		if (id == R.id.action_settings) {
+		if (id == R.id.action_sign_in) {
+			// set widget id's
+			//Toast.makeText(SignUpActivity.this, "Going to app1",
+				//	Toast.LENGTH_SHORT).show();
+			
+			
+
+			// create new parseUser object
+			ParseUser newUser = new ParseUser();
+
+			// get the strings stored in userName and password field
+
+			usernameText = userNameField.getText().toString();
+			emailText = emailField.getText().toString();
+			passwordText = passwordField.getText().toString();
+
+			// set parse User name and password fields
+			newUser.setUsername(usernameText);
+			newUser.setEmail(emailText);
+			newUser.setPassword(passwordText);
+
+				//signUpInBackground
+			newUser.signUpInBackground(new SignUpCallback() {
+				public void done(ParseException e) {
+					Toast.makeText(SignUpActivity.this, "Going to exception",
+					Toast.LENGTH_SHORT).show();
+					if (e == null) {
+						Toast.makeText(SignUpActivity.this, "Going to app",
+								Toast.LENGTH_SHORT).show();
+					} // if sigUp passed
+					else if (e.getCode() == ParseException.USERNAME_TAKEN) {
+						Toast.makeText(SignUpActivity.this,
+								"Username already taken", Toast.LENGTH_LONG)
+								.show();
+					}// userName is taken
+					else {
+						Toast.makeText(SignUpActivity.this,
+								e.getMessage(), Toast.LENGTH_LONG)
+								.show();
+					}
+				}
+			});
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
 	}
 }
+	
+

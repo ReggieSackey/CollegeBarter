0b229404c114ca685b843bb4d5adc7339a9ac02a
Larry Boateng Asante <boatenga17@grinnell.edu>
2014-09-13 14:29:04 -0500

    Add CartFragment

--- CollegeBarter/src/com/hacks/collegebarter/navdrawer/MainAppActivity.java
+++ CollegeBarter/src/com/hacks/collegebarter/navdrawer/MainAppActivity.java
@@ -46,10 +46,13 @@ public class MainAppActivity extends Activity
     @Override
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
+    	
         FragmentManager fragmentManager = getFragmentManager();
         fragmentManager.beginTransaction()
                 .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                .commit();
+                .commit(); 
+        
+      
     }
 
     public void onSectionAttached(int number) {
